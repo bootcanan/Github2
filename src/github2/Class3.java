c85a71929135be072a324b69734a2a4d002cc6c2
Canan Ozdemir <crazyfordata.123@gmail.com>
2021-02-28 17:38:46 +0000

    first message to local canan

--- src/github2/Class3.java
+++ src/github2/Class3.java
@@ -5,6 +5,7 @@ public class Class3 {
 	public static void main(String[] args) {
 	System.out.println("hello world 2");
 
+	System.out.println("hi everyone this is first commit on canan branch");
 	}
 
 }
